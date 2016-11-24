fb90b4ebbd0e1bff19ea7c679741b1942c891579
Vyacheslav Voronenko <git@voronenko.info>
2016-11-24 17:04:02 +0200

    exact versioning

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'git@voronenko.info'
 license 'MIT'
 description 'Installs/Configures postfix'
 long_description 'Installs/Configures postfix'
-version '0.1.2'
+version '0.1.3'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
@@ -14,4 +14,4 @@ issues_url 'https://github.com/softasap/sa-postfix-chef/issues' if respond_to?(:
 # gets a `View Source` link
 source_url 'https://github.com/softasap/sa-postfix-chef' if respond_to?(:source_url)
 
-depends 'sa-chef-lineinfile'
+depends 'sa-chef-lineinfile', '>= 0.0.1'
